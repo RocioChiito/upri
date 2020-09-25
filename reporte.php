dab4824af21ca2f208fb4139e03cb884d9fea483
Juan Fajardo <bett0@potosi.bo>
2020-09-25 14:25:45 -0400

    reporte inicio

---
+++ reporte.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <!-- CABECERA -->
+    <table width="100%">
+      <tr>
+        <td> <img src="" alt="imagen"> </td>
+        <td> <img src="" alt="imagen"> </td>
+        <td>
+          <table width="100%">
+            <tr>
+              <td><center>
+                <h2>UNIVERSIDAD MAYOR DE SAN ANDRÉS</h2>
+                <h4>FACULTAD DE DERECHO Y CIENCIAS POLÍTICAS<br>
+                    UNIDAD DE POSTGRADO Y RELACIONES INTERNACIONALES</h4>
+                </center>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p style="text-align: center;text-align: center;display: block;padding:20px;border:3px solid #326095;color:#326095;font-size: 25px;-webkit-border-radius: 10px;-moz-border-radius: 10px;border-radius: 10px;" ><b>FORMULARIO DE INSCRIPCIÓN</b> </p>
+              </td>
+            </tr>
+          </table>
+        </td>
+        <td> <b style="color:red; font-size:18px;"> N°0000683 </b> </td>
+      </tr>
+    </table>
+
+    <!-- CUERPO1 -->
+    <table width="100%">
+      <tr>
+        <td style="text-align: center;font-size: 20px;background-color:#326095; color:#fff; padding:20px;"> <b>INFORMACIÓN DEL PROGRAMA</b> </td>
+      </tr>
+      <tr>
+        <td style="padding:15px;">
+          <b>
+          Nombre del Programa::DOCTORADO EN DERECHO CONSTITUCIONAL Y ADMINISTRATIVO<br>
+          Nivel del Programa::DOCTORADO<br>
+          Version I<br>
+          Paralelo::A<br>
+          </b>
+        </td>
+      </tr>
+    </table>
+
+    <!-- CUERPO2 -->
+    <table width="100%" border="1">
+      <tr>
+        <td style="text-align: center;font-size: 20px;background-color:#326095; color:#fff; padding:20px;"> <b>DATOS PERSONALES DEL CURSANTE</b> </td>
+      </tr>
+      <tr>
+        <td style="padding:5px;">
+          <table width="100%" border="1">
+            <tr>
+              <td align="right" width="50%"> <b>Primer Apellido::</b> </td>  <td align="left">123</td>
+            </tr>
+            <tr>
+              <td align="right" width="50%"> <b>Segundo Apellido::</b> </td>  <td align="left">123</td>
+            </tr>
+            <tr>
+              <td align="right" width="50%"> <b>Nombre::</b> </td>  <td align="left">123</td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+    </table>
+    <!-- CUERPO3 -->
+    <table>
+    </table>
+
+    <!-- CUERPO4 -->
+    <table>
+    </table>
+
+    <!-- CUERPO5 -->
+    <table>
+    </table>
+
+    <!-- CUERPO6 -->
+    <table>
+    </table>
+
+    <!-- CUERPO7 -->
+    <table>
+    </table>
+
+
+  </body>
+</html>
